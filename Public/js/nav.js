76f96c687b6f9c8b935b7f44488174ba90c5246b
JustinS7 <86580642+JustinS7@users.noreply.github.com>
2021-12-16 14:48:52 -0500

    Add files via upload

---
+++ Public/js/nav.js
@@ -0,0 +1,24 @@
+const createNav = () => {
+  let nav = document.querySelector('.navbar');
+
+  nav.innerHTML = `
+  <div class="nav">
+  <div class="brandlogo">Just | Commerce</div>
+  <div class="nav-items">
+      <a href="#"><i class="fas fa-user"></i>
+      </a>
+      <a href="#"><i class="fas fa-shopping-cart"></i></i>
+      </a>
+    </div>
+  </div>
+</div>
+<ul class="links-container">
+  <li class="link-item"><a href="/Public/index.html" class="link">Home</a></li>
+  <li class="link-item"><a href="/Public/product.html" class="link">Women</a></li>
+  <li class="link-item"><a href="/Public/productm.html" class="link">Men</a></li>
+  <li class="link-item"><a href="/Public/productw.html" class="link">Accessories</a></li>
+  <li class="link-item"><a href="/Public/search.html" class="link"> | Search All</a></li>
+</ul>`;
+}
+
+createNav();
